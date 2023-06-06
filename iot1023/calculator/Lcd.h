2d24c3576004f9a35cc894f88cfa814b69b3f083
ngchongin <ngchongin@gmail.com>
2023-06-06 01:31:29 -0400

    update

--- iot1023/calculator/Lcd.h
+++ iot1023/calculator/Lcd.h
@@ -1,4 +1,5 @@
 #include <LiquidCrystal.h>
+#include "LcdDisplay.h"
 
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 const int ContrastValue = 100;
@@ -13,8 +14,7 @@ public:
     
     _current_row = 1;
     _current_col = 0;
-
-    initializeGraphics();
+    _lcd_grphic = new LcdGrphic(&lcd);
   }
 
   void UpdateDraw() 
@@ -30,108 +30,22 @@ public:
 
     for (int i = 7; i < 16; ++i)
     {
-      lcd.setCursor(i, _current_row);
-      lcd.print(i - 7);
+      _lcd_grphic->DrawNumber(i - 7, i, _current_row);
+  
       delay(1000);
     }
 }
+
 private:
-  void initializeGraphics() 
+  void InitOptions()
   {
-    _addition_graphic = new byte[8]{
-      B00000,   
-      B00100,
-      B00100,
-      B11111,
-      B11111,
-      B00100,
-      B00100,
-      B00000
-    };
-    
-    _subtraction_graphic = new byte[8]{
-      B00000,   
-      B00000,
-      B00000,
-      B11111,
-      B11111,
-      B00000,
-      B00000,
-      B00000
-    };
-
-    _multiplication_graphic = new byte[8]{
-      B00000,
-      B00000,
-      B10001,
-      B01010,
-      B00100,
-      B01010,
-      B10001,
-      B00000
-    };
-
-    _division_graphic = new byte[8]{
-      B00000,
-      B01100,
-      B00000,
-      B11111,
-      B00000,
-      B00000,
-      B01100,
-      B00000
-    };
-
-    _equal_graphic = new byte[8]{
-      B00000,
-      B00000,
-      B01110,
-      B00000,
-      B01110,
-      B00000,
-      B00000,
-      B00000
-    };
-
-    _left_bracket_graphic = new byte[8]{
-      B00010,
-      B00100,
-      B01000,
-      B01000,
-      B01000,
-      B00100,
-      B00010,
-      B00000
-    };
-
-    _right_bracket_graphic = new byte[8]{
-      B01000,
-      B00100,
-      B00010,
-      B00010,
-      B00010,
-      B00100,
-      B01000,
-      B00000
-    };
-
-    lcd.createChar(0, _addition_graphic);
-    lcd.createChar(1, _subtraction_graphic);
-    lcd.createChar(2, _multiplication_graphic);
-    lcd.createChar(3, _division_graphic);
-    lcd.createChar(4, _equal_graphic);
-    lcd.createChar(5, _left_bracket_graphic);
-    lcd.createChar(6, _right_bracket_graphic);
+    _symbol_option_size = 7;
+    _symbol_options = new char[7]{'+', '-', '*', '/', '=', '{', '}'};
   }
 private:
-  byte* _addition_graphic;
-  byte* _subtraction_graphic;
-  byte* _multiplication_graphic;
-  byte* _division_graphic;
-  byte* _equal_graphic;
-  byte* _left_bracket_graphic;
-  byte* _right_bracket_graphic;
-
+  char* _symbol_options;
+  int _symbol_option_size;
+  LcdGrphic* _lcd_grphic;
   int _current_row;
   int _current_col;
 };
---
+++ iot1023/calculator/LcdDisplay.h
@@ -0,0 +1,143 @@
+#include <LiquidCrystal.h>
+class LcdGrphic
+{
+public:
+  LcdGrphic(LiquidCrystal* lcd)
+  {
+    _lcd = lcd;
+    initializeGraphics();
+  }
+
+  void DrawPicture(char name, int col, int row)
+  {
+    _lcd->setCursor(col, row);
+    switch(name)
+    {
+      case '+':
+        _lcd->write(byte(0));
+        break;
+      case '-':
+        _lcd->write(byte(1));
+        break;
+      case '*':
+        _lcd->write(byte(2));
+        break;
+      case '/':
+        _lcd->write(byte(3));
+        break;
+      case '=':
+        _lcd->write(byte(4));
+        break;
+      case '{':
+        _lcd->write(byte(5));
+        break;
+      case '}':
+        _lcd->write(byte(6));
+        break;
+    }
+  }
+
+  void DrawNumber(int number, int col, int row)
+  {
+    _lcd->setCursor(col, row);
+    _lcd->print(number);
+  }
+
+private:
+  void initializeGraphics() 
+  {
+    _addition_graphic = new byte[8]{
+      B00000,   
+      B00100,
+      B00100,
+      B11111,
+      B11111,
+      B00100,
+      B00100,
+      B00000
+    };
+    
+    _subtraction_graphic = new byte[8]{
+      B00000,   
+      B00000,
+      B00000,
+      B11111,
+      B11111,
+      B00000,
+      B00000,
+      B00000
+    };
+
+    _multiplication_graphic = new byte[8]{
+      B00000,
+      B00000,
+      B10001,
+      B01010,
+      B00100,
+      B01010,
+      B10001,
+      B00000
+    };
+
+    _division_graphic = new byte[8]{
+      B00000,
+      B01100,
+      B00000,
+      B11111,
+      B00000,
+      B00000,
+      B01100,
+      B00000
+    };
+
+    _equal_graphic = new byte[8]{
+      B00000,
+      B00000,
+      B01110,
+      B00000,
+      B01110,
+      B00000,
+      B00000,
+      B00000
+    };
+
+    _left_bracket_graphic = new byte[8]{
+      B00010,
+      B00100,
+      B01000,
+      B01000,
+      B01000,
+      B00100,
+      B00010,
+      B00000
+    };
+
+    _right_bracket_graphic = new byte[8]{
+      B01000,
+      B00100,
+      B00010,
+      B00010,
+      B00010,
+      B00100,
+      B01000,
+      B00000
+    };
+
+    _lcd->createChar(0, _addition_graphic);
+    _lcd->createChar(1, _subtraction_graphic);
+    _lcd->createChar(2, _multiplication_graphic);
+    _lcd->createChar(3, _division_graphic);
+    _lcd->createChar(4, _equal_graphic);
+    _lcd->createChar(5, _left_bracket_graphic);
+    _lcd->createChar(6, _right_bracket_graphic);
+  }
+private:
+  LiquidCrystal* _lcd;
+  byte* _addition_graphic;
+  byte* _subtraction_graphic;
+  byte* _multiplication_graphic;
+  byte* _division_graphic;
+  byte* _equal_graphic;
+  byte* _left_bracket_graphic;
+  byte* _right_bracket_graphic;
+};
